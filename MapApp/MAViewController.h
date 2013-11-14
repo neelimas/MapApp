ad29e42ca792626e4dfcc2baf38696c6b876a5e6
neelimas <user@users-Mac-mini-127.local>
2013-11-14 13:43:53 -0600

    quickmapiosapp

--- MapApp/MAViewController.h
+++ MapApp/MAViewController.h
@@ -7,7 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface MAViewController : UIViewController
+@interface MAViewController : UIViewController <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+- (IBAction)zoom2Action:(id)sender;
+- (IBAction)typeAction:(id)sender;
+
 
 @end
--- MapApp/MAViewController.m
+++ MapApp/MAViewController.m
@@ -18,12 +18,37 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.mapView setShowsUserLocation:YES];
+    CLLocationCoordinate2D annotationCoord;
+    annotationCoord.latitude = 47.640071;
+    annotationCoord.longitude = -122.129598;
+    
+    MKPointAnnotation *ap = [[MKPointAnnotation alloc] init];
+    ap.coordinate = annotationCoord;
+    ap.title = @"Microsoft";
+    ap.subtitle = @"MS Headquaters";
+    [self.mapView addAnnotation:ap];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+- (IBAction)zoom2Action:(id)sender {
+  MKUserLocation *userLocation=   self.mapView.userLocation;
+ MKCoordinateRegion region =    MKCoordinateRegionMakeWithDistance(userLocation.location.coordinate, 50, 50);
+    [self.mapView setRegion:region animated:NO ];
+}
+
+- (IBAction)typeAction:(id)sender {
+    if(self.mapView.mapType == MKMapTypeStandard)
+        self.mapView.mapType = MKMapTypeSatellite;
+    else
+        self.mapView.mapType = MKMapTypeStandard;
 }
 
 @end
